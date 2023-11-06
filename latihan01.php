8b674dffba2f7a2df0f7eef5b065cffc980e9910
Hawayasin <yasinmanikhawa@mail.ugm.ac.id>
2023-11-06 21:17:50 +0700

    Run Jadi

--- latihan01.php
+++ latihan01.php
@@ -4,23 +4,15 @@
 <head>
     <!-- Metadata -->
     <meta charset="utf-8">
-    <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="author" content="DIVSIG UGM">
-    <meta name="description" content="leaflet basic">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Judul pada tab browser -->
     <title>LeafletJS</title>
     <!-- Leaflet CSS Library -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css">
-    <!-- Tab browser icon -->
-    <link rel="icon" type="image/x-icon" href="http://luk.staff.ugm.ac.id/logo/UGM/Resmi/Warna.gif">
     <style>
         /* Tampilan peta fullscreen */
-        html,
-        body,
-        #map {
+        html, body, #map {
             height: 100%;
             width: 100%;
             margin: 0px;
@@ -30,39 +22,18 @@
 
 <body>
     <!-- Leaflet JavaScript Library -->
-    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js">
-
-    </script>
+    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script>
     <div id="map"></div>
 
     <script>
         /* Initial Map note: sesuaikan setView koordinat dan zoom level ke titik tengah lembar peta*/
-        var map = L.map('map').setView([-1.850253, 118.876685], 5); //lat, long, zoom
+        var map = L.map('map').setView([-7.77, 110.37], 10); //lat, long, zoom
 
 
-        /* Tile Basemap note: pilihan basemap diakses pada https://leaflet-extras.github.io/leaflet-providers/preview/)*/
-        var basemap1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="DIVSIG UGM" target="_blank">DIVSIG UGM</a>' //menambahkan nama//
-        });
-        var basemap2 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri | <a href="Latihan WebGIS" target="_blank">DIVSIG UGM</a>'
-        });
-
-        var basemap3 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri | <a href="Lathan WebGIS" target="_blank">DIVSIG UGM</a>'
-        });
-
-        var basemap4 = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptile s.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        });
+   /* Tile Basemap */
+   var basemap1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="DIVSIG UGM" target="_blank">DIVSIG UGM</a>'});
         basemap1.addTo(map);
-        /* Control Layer */
-        var baseMaps = {
-            "OpenStreetMap": basemap1,
-            "Esri World Street": basemap2,
-            "Esri Imagery": basemap3,
-            "Stadia Dark Mode": basemap4
-        };
 
         /* Layer Marker */
         <?php
@@ -71,6 +42,7 @@
         $password = "";
         $dbname = "pgweb-8";
         $conn = new mysqli($servername, $username, $password, $dbname);
+
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -80,12 +52,13 @@
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                $lat = $row["latitude"];
-                $long = $row["Longitude"];
+                $lat = $row["Latitude"];
+                $long = $row["longitude"];
                 $info = $row["kecamatan"];
                 echo "L.marker([$lat, $long]).addTo(map).bindPopup('$info');";
             }
-        } else {
+        } 
+        else {
             echo "0 results";
         }
         $conn->close();
